e54c62ed6b7fc5b1089a7d1c85f7ae14a7ecad42
bmeyer55 <69869458+bmeyer55@users.noreply.github.com>
2020-12-14 15:16:04 -0500

    first commit

---
+++ Inputs-and-Variables.py
@@ -0,0 +1,46 @@
+'''This is the first program i will write that has inputs, 
+variables, and comments.
+12/09/20
+Bowen Meyer'''
+
+'''
+m = "I'm"
+n = "in"
+o = "Computer"
+p = "Science"
+q = str(1)
+r = "_"
+
+print(m + r + n + r + o + r + p + r + q )
+
+'''
+
+
+
+
+
+
+
+
+#calculates the perimeter of the area of the rectangle
+
+#for user input for length
+length = float(input("What is the Length of the rectangle? "))
+
+#ask user for width 
+width = float(input("What is the Width of the rectangle? "))
+
+#calculates using user inputs for permeter
+perimeter = (2 * length) + (2 * width)
+
+#calculates using user inputs for area
+area = length * width
+
+#prints perimeter tags
+print("Perimeter")
+print(perimeter)
+
+#prints area tags
+print("Area")
+print(area)
+
---
+++ Madlib.py
@@ -0,0 +1,38 @@
+# An attempt at making a functional Madlib
+
+#evey input command for mad lib
+noun_1 = input("Noun? ")
+place_1 = input("Place? ")
+noun_2 = input("Noun? ")
+noun_3 = input("Noun? ")
+number_1 = input("Pick a number. ")
+noun_4 = input("Noun? ")
+emotion_1 = input("Choose an emotion. ")
+plural_noun_1 = input("Plural Noun? ")
+verb_1 = input("Verb? ")
+emotion_2 = input("Emotion? ")
+noun_5 = input("Noun? ")
+place_2 = input("Place?")
+family_member_1 = input("Pick a family member. ")
+article_of_clothing = input("Choose a piece of clothing. ")
+family_member_2 = input("Choose another family member. ")
+family_member_3 = input("One more family member. ")
+s = " "
+
+
+print("Kronos and his "+ noun_1 + "."" Kronos and Rhea ruled over the "
+ + place_1 + ". " + noun_2 + " was their power. ")
+print(" One day, when Rhea was about to have her first "
++ noun_3 + ", her mother told Kronos that "
++ number_1 + " of his children would become the ruler of the "
++ noun_4 + ". Kronos was not "
++ emotion_1 + ". He decided if his "
++ plural_noun_1 + " were going to usurp his throne, he would "
++ verb_1 + " them first. So he did! Rhea was not too "
++ emotion_2 + " about this, she managed to save her last "
++ noun_5 + " Zues. He grew up in a "
++ place_2 + ", hidden from his "
++ family_member_1 + ". But when he was old enough, he put on a(n) "
++ article_of_clothing + " and made him vomit up all his bothers and "
++ family_member_2 + ". His planned worked! Zues and his "
++ family_member_3 + " were reunited")
